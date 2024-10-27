df1c2aaf9547404a5aa1725322c0a793c0241550
Devansh <devanshbhargava90@gmail.com>
2024-10-27 21:51:59 +0530

    html:value & type attribute in all buttons added AND JS:displaying number when clicked function added

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-function add(a, b,) {
-    return a+b;
-}
+document.addEventListener('DOMContentLoaded',function(){
+    function add(a, b,) {
+        return a+b;
+    }
 
-function multiply (a,b){
-    return a*b;
-}
+    function multiply (a,b){
+        return a*b;
+    }
 
-function subtract (a,b){
-    return a-b;
-}
+    function subtract (a,b){
+        return a-b;
+    }
 
-function divide (a,b){
-    return a/b;
-}
+    function divide (a,b){
+        return a/b;
+    }
 
-function operate(a,b,operator){
-    return operator(a,b);
-}
+    function operate(a,b,operator){
+        return operator(a,b);
+    }
+    let displayedAndClickedNumber = 0;
+
+    const displayContent = document.getElementById("screenDisplay");
+    displayContent.textContent = `${displayedAndClickedNumber}`;
+
+    const numberKeys = document.querySelectorAll(".number");
+
+    numberKeys.forEach((item) => {
+        item.addEventListener("click",(event)=>{
+            displayedAndClickedNumber = event.target.value;
+            displayContent.textContent = `${displayedAndClickedNumber}`;
+        })
+
+
+    });
+
+
+
+})
